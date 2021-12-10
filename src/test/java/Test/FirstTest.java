2771bdf9f7581f28aa56e83234b9bb7ca2db60e1
alex-sotnikov89 <71437753+alex-sotnikov89@users.noreply.github.com>
2021-12-10 17:29:34 +0300

    Update FirstTest.java

--- src/test/java/Test/FirstTest.java
+++ src/test/java/Test/FirstTest.java
@@ -3,26 +3,22 @@ package Test;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.time.Duration;
-import static org.junit.Assert.fail;
+
 
 public class FirstTest {
     private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
 
     @Before
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "tools\\chromedriver.exe");
         driver = new ChromeDriver();
-        baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
     }
 
     @Test
     public void test() throws Exception {
-        driver.get(baseUrl);
         driver.get("http://172.17.52.10/account/login?ReturnUrl=%2F");
         driver.findElement(By.id("username")).click();
         driver.findElement(By.id("username")).clear();
@@ -32,47 +28,19 @@ public class FirstTest {
         driver.findElement(By.id("cbrememberme")).click();
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         driver.findElement(By.xpath("//div[@id='header-title']/a/h1")).click();
+        driver.findElement(By.xpath("//ul[@id='topmenu']/li[3]/span")).click();
+//        driver.findElement(By.xpath("//a[@href='/DTO_Info_Document/viewlist']")).click();
+        driver.findElement(By.linkText("Реестр документов")).click();
+        driver.findElement(By.linkText("Создать")).click();
+        driver.findElement(By.id("info_document_edit_docno")).click();
+        driver.findElement(By.id("info_document_edit_docno")).clear();
+        driver.findElement(By.id("info_document_edit_docno")).sendKeys("Тест1");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.linkText("Назад")).click();
     }
 
-    @After
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
+//    @After
+//    public void tearDown() throws Exception {
+//        driver.quit();
+//    }
 }
